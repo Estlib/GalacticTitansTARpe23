24aa5e1fd56d194b3a914085af6e29d7cd164156
Estlib <75094683+Estlib@users.noreply.github.com>
2025-11-09 09:41:15 +0200

    Added developer resources for dev usage under admin privileges in the footer, but for normal user or user who is not logged in, terms of usage link is displayed instead. also some terms in that view

--- GalacticTitans/Controllers/HomeController.cs
+++ GalacticTitans/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace GalacticTitans.Controllers
         {
             return View();
         }
+        public IActionResult ResourceListing()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
